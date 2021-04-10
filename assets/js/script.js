4b3d8e6049965ad898423988964f8c9be48b14cf
unknown <tgelety@gmail.com>
2021-04-09 17:02:28 -0700

    more button stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,53 @@
-// to write to the DOM 
+var scoreCount = 0;
+// simplifications 
+
 var body = document.body;
-// create element 
+// page elements 
 var h1El = document.createElement('h1');
-// modify element 
-h1El.textContent = "START HERE --->";
-h1El.setAttribute('style', 'text-align: center');
+var h2El = document.createElement('h2');
 var startBtn = document.createElement("button");
+var correctBtn = document.createElement("button");
+var incorrectBtn = document.createElement("button");
+
+// attributes 
+h1El.setAttribute('style', 'text-align: center');
+correctBtn.setAttribute('id', 'correct');
+incorrectBtn.setAttribute('id','incorrect');
+
+// text content 
 startBtn.innerHTML = "START";
+h1El.textContent = "START HERE --->";
+h2El.textContent = "click a button";
+correctBtn.innerHTML = "CORRECT";
+incorrectBtn.innerHTML = "INCORRECT";
+
 
 // append to page 
 body.appendChild(h1El);
 h1El.appendChild(startBtn);
 
-questionEl = function() {
+ var questionEl = function() {
     h1El.textContent = "Question";
-    var h2El = document.createElement('h2');
-    h2El.textContent = "click a button";
-    var correctBtn = document.createElement("button");
-    correctBtn.innerHTML = "CORRECT";
-    var incorrectBtn = document.createElement("button");
-    incorrectBtn.innerHTML = "INCORRECT";
     body.appendChild(h2El);
     body.appendChild(correctBtn);
     body.appendChild(incorrectBtn);
     // target the thing we are clicking on 
-var correctAnswer = document.querySelector("button");
+    var correctAnswer = document.querySelector("#correct");
+    // click it to change the html elements
+    correctAnswer.addEventListener('click', answerRight);
+    var incorrectAnswer = document.querySelector("#incorrect");
 // click it to change the html elements
-correctAnswer.addEventListener('click', questionEl);
-
-
+incorrectAnswer.addEventListener('click', answerWrong);
 };
 
+var answerRight = function() {
+    scoreCount++;
+    console.log(scoreCount)
+};
+var answerWrong = function() {
+    incorrectBtn.innerHTML = "CLICKED";
+};
 // target the thing we are clicking on 
 var startQuiz = document.querySelector("button");
 // click it to change the html elements
 startQuiz.addEventListener('click', questionEl);
-
-var scoreCount = 0;
