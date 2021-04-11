79a5904c02f8513896900e9a71be127cff0928d4
unknown <tgelety@gmail.com>
2021-04-10 17:14:24 -0700

    working on localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,10 @@ var questionNumber = 1;
 var titleEl = document.createElement('h1');
 var mathQuestionEl = document.createElement('h2');
 var timerEl = document.createElement('h3');
+var playerName = document.createElement('input')
 var startBtn = document.createElement("button");
+var highScoresBtn = document.createElement("button");
+var saveBtn = document.createElement("button");
 var aBtn = document.createElement("button");
 var bBtn = document.createElement("button");
 var cBtn = document.createElement("button");
@@ -21,8 +24,10 @@ aBtn.setAttribute('id', 'a');
 bBtn.setAttribute('id', 'b');
 cBtn.setAttribute('id', 'c');
 dBtn.setAttribute('id', 'd');
-startBtn.setAttribute('id', 'start')
+highScoresBtn.setAttribute('id', 'highscore');
+startBtn.setAttribute('id', 'start');
 
+saveBtn.innerHTML = "SAVE"
 // text content 
 startBtn.innerHTML = "START";
 titleEl.textContent = "START HERE --->";
@@ -42,27 +47,27 @@ var questionEl = function () {
     titleEl.textContent = "Question " + questionNumber;
     timerGame()
     randomMath()
-    
+    startQuiz.classList.add("hide")
     // target the correct id  
     var correctAnswer = document.querySelector("#a");
     // click it to run answerRight function
     correctAnswer.addEventListener('click', answerRight);
-    
+
     // target the incorrect id 
     var incorrectAnswer = document.querySelector("#b");
     // click it to run answerWrong function
     incorrectAnswer.addEventListener('click', answerWrong);
-    
+
     // target the incorrect id 
     var incorrectAnswer = document.querySelector("#c");
     // click it to run answerWrong function
     incorrectAnswer.addEventListener('click', answerWrong);
-    
+
     // target the incorrect id 
     var incorrectAnswer = document.querySelector("#d");
     // click it to run answerWrong function
     incorrectAnswer.addEventListener('click', answerWrong);
-    
+
 };
 
 var timerGame = function () {
@@ -70,7 +75,7 @@ var timerGame = function () {
     var timeInterval = setInterval(function () {
         if (timeCount > -1) {
             timerEl.textContent = "TIME LEFT: " + timeCount + " seconds";
-            
+
             timeCount--;
         } else {
             clearInterval(timeInterval)
@@ -93,6 +98,7 @@ var randomMath = function () {
     body.appendChild(bBtn);
     body.appendChild(cBtn);
     body.appendChild(dBtn);
+
 }
 
 var answerRight = function () {
@@ -100,23 +106,43 @@ var answerRight = function () {
     questionNumber++;
     randomMath()
     titleEl.textContent = "Question " + questionNumber;
-    console.log(scoreCount);
 };
 
 var answerWrong = function () {
-    randomMath ()
+    randomMath()
     timeCount = timeCount - 5;
     questionNumber++;
+    titleEl.textContent = "Question " + questionNumber;
 };
 
 var endGame = function () {
     titleEl.textContent = "GAMEOVER"
     mathQuestionEl.textContent = "SCORE: " + scoreCount;
+    // TODO: make a prompt box to make a userName
+    playerName.setAttribute('type', 'text', 'id', 'userName');
+    body.appendChild(playerName);
+    playerName.appendChild(saveBtn)
+    //  var saveScore = localStorage.setItem(userName,scoreCount);
+    //  console.log(saveScore);
     body.removeChild(aBtn);
     body.removeChild(bBtn);
     body.removeChild(cBtn);
     body.removeChild(dBtn);
-    
+    highScoresBtn.innerHTML = "HIGHSCORES"
+    body.appendChild(highScoresBtn);
+    var highScores = document.querySelector("#highscore");
+    highScores.classList.remove("hide");
+    highScores.addEventListener('click', function () {
+        questionNumber = 1;
+        scoreCount = 0;
+        titleEl.textContent = "HIGHSCORES";
+        mathQuestionEl.textContent = "HALL OF FAME:"
+        highScores.classList.add("hide");
+        startQuiz.classList.remove("hide");
+        startQuiz.textContent = "PLAY AGAIN"
+        body.appendChild(startBtn);
+    })
+
 }
 
 // target the thing we are clicking on 
